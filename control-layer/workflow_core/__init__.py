29abb2b88b89d991f412792fe11b0f6b1079a258
Maxbry <maxbry123@gmail.com>
2026-08-06 18:29:49 -0500

    feat(workflow_core): export DAG/Sheriff from __init__.py

--- control-layer/workflow_core/__init__.py
+++ control-layer/workflow_core/__init__.py
@@ -2,8 +2,13 @@ from .contracts import (
     Checkpoint, ChangeProposal, Failure, Goal,
     NodeDefinition, NodeRuntime, WorkflowDefinition,
 )
+from .dag import DAGDefinition
+from .dag_patch import DAGPatch, DAGPatchEngine
+from .dag_validator import DAGValidator
 from .enums import EventType, NodeStatus, WorkflowStatus
 from .events import WorkflowEvent
+from .policies import WorkflowPolicy
+from .sheriff import DeterministicSheriff, SheriffDecision, SheriffContract
 from .state import WorkflowState
 from .state_machine import WorkflowStateMachine
 from .store import InMemoryWorkflowStore, WorkflowStore
@@ -11,7 +16,9 @@ from .store import InMemoryWorkflowStore, WorkflowStore
 __all__ = [
     "Checkpoint", "ChangeProposal", "Failure", "Goal",
     "NodeDefinition", "NodeRuntime", "WorkflowDefinition",
+    "DAGDefinition", "DAGPatch", "DAGPatchEngine", "DAGValidator",
     "EventType", "NodeStatus", "WorkflowStatus",
     "WorkflowEvent", "WorkflowState", "WorkflowStateMachine",
+    "WorkflowPolicy", "DeterministicSheriff", "SheriffDecision", "SheriffContract",
     "WorkflowStore", "InMemoryWorkflowStore",
 ]
