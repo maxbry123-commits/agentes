11145833b75350cead86e7bfdb14b02ad89a6628
Maxbry <maxbry123@gmail.com>
2026-08-06 21:36:00 -0500

    feat(loops): export Fase4+5 modules

--- control-layer/loops/__init__.py
+++ control-layer/loops/__init__.py
@@ -3,6 +3,16 @@ from .state_machine import StateMachine
 from .phases import PhaseRunner, Sheriff
 from .recovery import RecoveryEngine
 from .policy import PolicyEngine, PolicyInput
+from .registry import LoopRegistry
+from .supervisor import LoopSupervisor, SupervisorConfig
+from .dlq import DeadLetterQueue, DLQItem
+from .lease import LeaseManager
+from .heartbeat import HeartbeatMonitor
+from .strategy_memory import StrategyMemory, StrategyRecord
+from .result_cache import ResultCache, fingerprint
+from .budget_governor import BudgetGovernor
+from .progress import ProgressEvaluator, AdaptiveIterationController
+from .risk import RiskEngine, HumanGate
 
 __all__ = [
     "LoopEngine",
@@ -13,4 +23,20 @@ __all__ = [
     "RecoveryEngine",
     "PolicyEngine",
     "PolicyInput",
+    "LoopRegistry",
+    "LoopSupervisor",
+    "SupervisorConfig",
+    "DeadLetterQueue",
+    "DLQItem",
+    "LeaseManager",
+    "HeartbeatMonitor",
+    "StrategyMemory",
+    "StrategyRecord",
+    "ResultCache",
+    "fingerprint",
+    "BudgetGovernor",
+    "ProgressEvaluator",
+    "AdaptiveIterationController",
+    "RiskEngine",
+    "HumanGate",
 ]
