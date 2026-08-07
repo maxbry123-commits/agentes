b2fc50dfdd2d00aae641d289c9e3847ec44b67cf
Maxbry <maxbry123@gmail.com>
2026-08-06 21:43:42 -0500

    feat(loops): export P2 modules

--- control-layer/loops/__init__.py
+++ control-layer/loops/__init__.py
@@ -13,30 +13,20 @@ from .result_cache import ResultCache, fingerprint
 from .budget_governor import BudgetGovernor
 from .progress import ProgressEvaluator, AdaptiveIterationController
 from .risk import RiskEngine, HumanGate
+from .detectors import NativeDetectors
+from .replay import EventReplayer
+from .simulator import LoopSimulator, ChaosMonkey
+from .metrics import LoopMetrics
+from .capability_router import CapabilityRouter
+from .persistence_store import PersistenceStore
 
 __all__ = [
-    "LoopEngine",
-    "LoopRunResult",
-    "StateMachine",
-    "PhaseRunner",
-    "Sheriff",
-    "RecoveryEngine",
-    "PolicyEngine",
-    "PolicyInput",
-    "LoopRegistry",
-    "LoopSupervisor",
-    "SupervisorConfig",
-    "DeadLetterQueue",
-    "DLQItem",
-    "LeaseManager",
-    "HeartbeatMonitor",
-    "StrategyMemory",
-    "StrategyRecord",
-    "ResultCache",
-    "fingerprint",
-    "BudgetGovernor",
-    "ProgressEvaluator",
-    "AdaptiveIterationController",
-    "RiskEngine",
-    "HumanGate",
+    "LoopEngine", "LoopRunResult", "StateMachine", "PhaseRunner", "Sheriff",
+    "RecoveryEngine", "PolicyEngine", "PolicyInput", "LoopRegistry",
+    "LoopSupervisor", "SupervisorConfig", "DeadLetterQueue", "DLQItem",
+    "LeaseManager", "HeartbeatMonitor", "StrategyMemory", "StrategyRecord",
+    "ResultCache", "fingerprint", "BudgetGovernor", "ProgressEvaluator",
+    "AdaptiveIterationController", "RiskEngine", "HumanGate",
+    "NativeDetectors", "EventReplayer", "LoopSimulator", "ChaosMonkey",
+    "LoopMetrics", "CapabilityRouter", "PersistenceStore",
 ]
