aaadc980b7c9d09168943a1f8ecbf29c1bfa9d58
Maxbry <maxbry123@gmail.com>
2026-08-10 02:16:01 -0500

    A-AUD-02: export DocumentTruthStore from engine __init__

--- extensions/audit_forensic/engine/__init__.py
+++ extensions/audit_forensic/engine/__init__.py
@@ -1,4 +1,10 @@
 # audit_forensic.engine
 from .packet_normalizer import normalize_packet, PacketError
+from .doc_truth import DocumentTruthStore, DocTruthError
 
-__all__ = ["normalize_packet", "PacketError"]
+__all__ = [
+    "normalize_packet",
+    "PacketError",
+    "DocumentTruthStore",
+    "DocTruthError",
+]
