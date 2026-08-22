1406cf3cbcf5c23b52a6f66e07bb9ceeb446e277
Maxbry <maxbry123@gmail.com>
2026-08-22 09:55:38 -0500

    feat(github_deploy): export remote_ops CRUD API for Cuenta B

--- extensions/github_deploy/__init__.py
+++ extensions/github_deploy/__init__.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 from .apply_push import apply_and_push, apply_from_payload
 from .deployer import DeployError, GitHubDeployer, load_deploy_config
+from .remote_ops import (
+    CUENTA_B_OWNER,
+    create_repo,
+    delete_paths,
+    get_file,
+    get_head,
+    identify_cuenta_b,
+    list_repos,
+    list_tree,
+    remote_op,
+    verify_file,
+    verify_head,
+    write_files,
+)
 
 __all__ = [
     "DeployError",
@@ -8,4 +22,16 @@ __all__ = [
     "load_deploy_config",
     "apply_and_push",
     "apply_from_payload",
+    "CUENTA_B_OWNER",
+    "identify_cuenta_b",
+    "get_head",
+    "get_file",
+    "list_tree",
+    "list_repos",
+    "write_files",
+    "delete_paths",
+    "verify_head",
+    "verify_file",
+    "create_repo",
+    "remote_op",
 ]
