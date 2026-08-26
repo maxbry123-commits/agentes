7c234dc5845fce76099da12ea3e47736f58c5442
Maxbry <maxbry123@gmail.com>
2026-08-26 04:49:00 -0500

    G2 refactoria: source provenance for schema extraction

---
+++ Refactoria/G2/source/programming_pipeline.py
@@ -0,0 +1,3 @@
+"""Source provenance for G2; canonical blob SHA 44dfb7a04474d4a4ee46662f7e40c60c7aa7e321."""
+SOURCE = "extensions/wordflow/engine/programming_pipeline.py"
+BLOB_SHA = "44dfb7a04474d4a4ee46662f7e40c60c7aa7e321"
