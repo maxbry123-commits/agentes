be2c53e90d0f44422847a5b32ba96d77714a43a0
Maxbry <maxbry123@gmail.com>
2026-09-02 21:02:12 -0500

    feat(yaiwes): add parallel evolution microkernel package

---
+++ agente-yaiwes/multi-workflow-engine/instances/source-evolution-workflow/microkernel/__init__.py
@@ -0,0 +1,5 @@
+"""Bounded parallel activation kernel for YAIWES evolution workflows."""
+
+from .parallel_kernel import KernelReport, LaneResult, ParallelEvolutionKernel
+
+__all__ = ["KernelReport", "LaneResult", "ParallelEvolutionKernel"]
