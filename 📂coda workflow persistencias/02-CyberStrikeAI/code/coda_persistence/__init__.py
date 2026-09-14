b77bedc2ff16bf3b1e06bf089023468b3ea7073f
Maxbry <maxbry123@gmail.com>
2026-09-14 00:41:12 -0500

    refactor(coda-link02): add safe persistence package

---
+++ 📂coda workflow persistencias/02-CyberStrikeAI/code/coda_persistence/__init__.py
@@ -0,0 +1,3 @@
+"""Benign CODA persistence adapter for transformed CyberStrikeAI."""
+from .link import PersistenceWorkflowLink, CheckpointStore, SafeTaskAdapter
+__all__ = ["PersistenceWorkflowLink", "CheckpointStore", "SafeTaskAdapter"]
