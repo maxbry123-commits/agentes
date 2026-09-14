986c4fb5287fcb16f2189a04828fab11aab1b4f5
Maxbry <maxbry123@gmail.com>
2026-09-14 00:43:04 -0500

    refactor(coda-link03): add safe persistence package

---
+++ 📂coda workflow persistencias/03-LuaN1aoAgent/code/coda_persistence/__init__.py
@@ -0,0 +1,3 @@
+"""Benign CODA persistence adapter for transformed LuaN1aoAgent."""
+from .link import PersistenceWorkflowLink, CheckpointStore, SafeTaskAdapter
+__all__ = ["PersistenceWorkflowLink", "CheckpointStore", "SafeTaskAdapter"]
